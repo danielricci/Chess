66b457b6d5b2d0b1b80ef048802f3e94ec5759b5
Daniel Ricci <thedanny09@gmail.com>
2017-01-18 16:16:50 -0500

    Testing commit

--- Chess/game/views/BaseView.java
+++ Chess/game/views/BaseView.java
@@ -49,6 +49,7 @@ public abstract class BaseView extends JPanel implements IView {
 	public BaseView(final BaseController controller) {			
 		this();
 		_controllers.add(controller);
+		this();
 	}
 	
 	public <T extends BaseController> BaseView(Class<T> controller) {
