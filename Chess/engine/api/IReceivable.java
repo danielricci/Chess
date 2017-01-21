d228be59ef9c3918bb426e6337fcd49fc85f107e
danielricci <danielricci@DESKTOP-P6NMRVE>
2017-01-20 23:44:58 -0500

    fix for build

--- Chess/engine/api/IReceivable.java
+++ Chess/engine/api/IReceivable.java
@@ -6,6 +6,19 @@ import java.util.Map;
 import communication.internal.dispatcher.DispatcherOperation;
 
 public interface IReceivable {
+	
+	/**
+	 * Executes the specified operation based on the operations registered by the IReceivable entity
+	 * 
+	 * @param sender The sender
+	 * @param operation The operation
+	 */
 	public void executeRegisteredOperation(Object sender, DispatcherOperation operation);
+	
+	/**
+	 * Gets the list of registered operation by the entity
+	 * 
+	 * @return Map<DispatcherOperation, ActionListener>
+	 */
 	public Map<DispatcherOperation, ActionListener> getRegisteredOperations();
 }
--- Chess/engine/api/IView.java
+++ Chess/engine/api/IView.java
@@ -2,7 +2,7 @@ package api;
 
 import models.GameModel;
 
-public interface IView extends IReceivable, IDestructable {
+public interface IView extends IDestructable, IReceivable {
 	public void render();
 	public void refresh(GameModel model);
 	public void register();
